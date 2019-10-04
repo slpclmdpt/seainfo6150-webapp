44afbb17d32122de6483ac6de5f6d7143d91cad3
unknown <du.pen@husky.neu.edu>
2019-10-04 19:22:54 -0400

    week-5

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types'
 import ArticleListItem from "./ArticleListItem.jsx"
+import SlugBotton from "./SlugButton.jsx"
+import styles from "./ArticleList.module.css";
 
 const ArticleList = props => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {props.articles.map(article => (
-        <li key={article.slug}><ArticleListItem article={article}/></li>
+        <li className={styles.section} key={article.slug}><ArticleListItem article={article}/></li>
       ))}
     </ul>
   );
